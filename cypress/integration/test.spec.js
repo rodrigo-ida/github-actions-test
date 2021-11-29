49a955d97d73bc460583850d2c7f14ef51c719aa
Rodrigo Ida <rodrigoida1@hotmail.com>
2021-11-29 10:51:25 -0300

    add teste.yml

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('testing the title', () => {
+    it('test the title', () => {
+        cy.visit('/')
+        cy.get('#title').invoke('html').should('eq', 'titulo')
+    }) 
+})
